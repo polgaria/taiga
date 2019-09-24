2b00ad5d84ca7b6d7b25d458aa410601bdb83736
geni <me@geni.site>
2019-09-24 20:48:14 +0300

    fix round to dec places function

--- include/taiga/util/Math.hpp
+++ include/taiga/util/Math.hpp
@@ -9,7 +9,8 @@ class Math {
 	template <typename T, typename = typename std::enable_if<
 							  std::is_arithmetic<T>::value, T>::type>
 	static T round_to_dec_places(T nonrounded, int dec_places) {
-		return std::round(nonrounded * (10 ^ dec_places)) / (10 ^ dec_places);
+		return std::round(nonrounded * std::pow(10, dec_places) /
+						  std::pow(10, dec_places));
 	}
 };
 }  // namespace Taiga::Util
