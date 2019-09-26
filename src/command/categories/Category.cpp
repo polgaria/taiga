14aa52c5c545ba8526635520b966563214a16e00
geni <me@geni.site>
2019-09-26 18:48:22 +0300

    clean up Category

--- src/command/categories/Category.cpp
+++ src/command/categories/Category.cpp
@@ -1,4 +1,4 @@
 #include <taiga/command/categories/Category.hpp>
 
-Taiga::Command::Category::Category(const std::string& name) : name(name) {}
-Taiga::Command::Category::~Category() {}
+Taiga::Command::Category::Category(std::string name) : name(std::move(name)) {}
+Taiga::Command::Category::~Category() = default;
