62f880a0c01b7b5cf6d3ece2e64ea25741bd48ee
geni <me@geni.site>
2019-10-16 19:03:45 +0300

    install mongocxx

---
+++ .travis/install-mongoc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -ex
+
+wget https://github.com/mongodb/mongo-c-driver/releases/download/${MONGOC_VER}/mongo-c-driver-${MONGOC_VER}.tar.gz
+tar xzf mongo-c-driver-${MONGOC_VER}.tar.gz
+
+cd mongo-c-driver-${MONGOC_VER} && mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
+sudo make install
---
+++ .travis/install-mongocxx.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -ex
+
+git clone https://github.com/mongodb/mongo-cxx-driver.git \
+    --branch releases/stable --depth 1
+cd mongo-cxx-driver/build
+
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
+make && sudo make install
